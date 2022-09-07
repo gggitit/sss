82545852e1d6f78f5f96608f6b09de46876c87db
gggitit <103881220+gggitit@users.noreply.github.com>
2022-09-07 10:17:12 +0800

    Update comic.js

--- comic.js
+++ comic.js
@@ -1,3 +1,3 @@
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://ghproxy.com/https://raw.githubusercontent.com/gggitit/sss/main/comic.css" type="text/css">');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/gggitit/sss@xXx/comic.css" type="text/css">');
 $done({ body });
